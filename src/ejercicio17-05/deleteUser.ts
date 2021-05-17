27f0057407ee572fd9d36a5c00ae27cef20abd38
alu0101203003 <alu0101203003@ull.edu.es>
2021-05-17 10:33:27 +0100

    ej 17-05: todo funciona

---
+++ src/ejercicio17-05/deleteUser.ts
@@ -0,0 +1,27 @@
+import {MongoClient} from 'mongodb';
+
+const dbURL = 'mongodb://127.0.0.1:27017';
+const dbName = 'users';
+
+interface User {
+    nombre: string, 
+    apellidos: string, 
+    edad: number,
+    email: string,
+    contraseña: string
+}
+
+MongoClient.connect(dbURL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then((client) => {
+  const db = client.db(dbName);
+
+  return db.collection<User>('users').deleteOne({
+    email: "alu0202@gmail.com",
+  });
+}).then((result) => {
+  console.log(result.deletedCount);
+}).catch((error) => {
+  console.log(error);
+});
